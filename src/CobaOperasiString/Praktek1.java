192a3855cd51884a685e3e98f910d22f0b10db43
User <User@User>
2018-08-13 20:41:39 -0800

    CobaOperasiString

--- src/CobaOperasiString/Praktek1.java
+++ src/CobaOperasiString/Praktek1.java
@@ -41,7 +41,45 @@ public class Praktek1 {
         System.out.println("perbandingan isi x dengan z (Case Sensitive): " + x.compareTo(z));
         System.out.println("perbandingan isi x dengan r (Case Sensitive): " + x.compareToIgnoreCase(r));
         
+        String s = "operasI";
+        System.out.println("isi variable s : "+ s);
+        System.out.println("perbandingan isi r dengan s (Case Sensitive) : " + r.compareTo(s));
         
+        System.out.println("pada terdapat huruf 'a' : " + x.contains("a"));
+        
+        System.out.println("isi variable x besar semua : "+ x.toUpperCase());
+        System.out.println("isi variable x kecil semua : "+ x.toLowerCase());
+        
+        
+        String t = "    operasi string      ";
+        System.out.println("isi variable t : \""+ t +"\"");
+        System.out.println("isi variable t tanpa spasi : \""+t.trim() +"\"");
+        
+        System.out.println("gabungkan isi variable x dan r : "+ x.concat(r));
+        
+        System.out.println("karakter mulai posisi 3 di variable x : " + x.substring(3));
+        
+        System.out.println("karakter dari posisi 3-5 di variable x : " + x.substring(3, 6));
+        
+        String tclean = t.trim();
+        System.out.println("isi variable tclean : "+ t.trim());
+        System.out.println("rubah '1' jadi 'E' di variable tclean : " + tclean.replace("i","E"));
+        
+        String[] arrT = tclean.split(" ");
+        System.out.println("pecah kata di variable tclean : ");
+        for(int i=0;i<arrT.length;i++)
+            System.out.println(arrT[i]);
+        
+        String[] arrTr = tclean.split("r");
+        System.out.println("pecah berdasarkan huruf 'r' di variable tclean : ");
+        for(int i=0;i<arrTr.length;i++)
+            System.out.println(arrTr[i]);
+        
+        double d = 1545454000;
+                System.out.println(String.format("Uang saya %,.4f", d));
+                
+                System.out.println(String.format(Locale.forLanguageTag("in-ID"), "Uang saya Rp %,.2f", d));
+                
                 
     }
     
