c3565a9a3ed182493f95b5d5332ca40c68c884b7
User <User@User>
2018-08-13 20:28:38 -0800

    Initial Commit

--- src/CobaOperasiString/Praktek1.java
+++ src/CobaOperasiString/Praktek1.java
@@ -16,16 +16,7 @@ public class Praktek1 {
         String identitas = "Muhammad Bagas Ramadhan / XRPL2 / 21";
         System.out.println("Identitas : "+ identitas);
    
-        String x = "Operasi";
-        System.out.println("isi variabel  x : " + x);
-        System.out.println("\""+ x + "\" panjang karakter = "+ x.length());
-        
-        System.out.println("x adalah kosong : " + x.isEmpty());
-        
-        String y = "";
-        System.out.println("isi variabel y : "+ y);
-        System.out.println("y adalah kosong : " + y.isEmpty());
-        
+      
        
     }
     
