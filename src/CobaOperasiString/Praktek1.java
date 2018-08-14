462ffd1089f46a739bfae0d6533b387368c5c5ee
User <User@User>
2018-08-14 03:31:14 -0800

    Coba method format

--- src/CobaOperasiString/Praktek1.java
+++ src/CobaOperasiString/Praktek1.java
@@ -75,7 +75,10 @@ public class Praktek1 {
         for(int i=0;i<arrTr.length;i++)
             System.out.println(arrTr[i]);
         
-        
+        double d = 1545454000;
+                System.out.println(String.format("Uang saya %,.4f", d));
+                
+                System.out.println(String.format(Locale.forLanguageTag("in-ID"), "Uang saya Rp %,.2f", d));
     }
     
 }
