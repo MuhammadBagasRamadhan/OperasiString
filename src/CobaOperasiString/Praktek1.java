fc9261cbb1cd5cb303722261a953a2e61c3a4aa7
User <User@User>
2018-08-13 20:16:04 -0800

    Initial Commit

---
+++ src/CobaOperasiString/Praktek1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CobaOperasiString;
+
+import java.util.Locale;
+
+/**
+ *
+ * @author User
+ */
+public class Praktek1 {
+    public static void main(String[] args) {
+        String identitas = "Muhammad Bagas Ramadhan / XRPL2 / 21";
+        System.out.println("Identitas : "+ identitas);
+   
+        
+                
+    }
+    
+}
