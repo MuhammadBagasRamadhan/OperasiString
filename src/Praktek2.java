315f96acb95aa71a5d82110a6b9b24cb4d535ed3
User <User@User>
2018-08-14 03:52:03 -0800

    Tambah tampil hasil

--- src/Praktek2.java
+++ src/Praktek2.java
@@ -40,7 +40,18 @@ public class Praktek2 {
         
         for(int i=0;i<arrKal.length;i++)
             System.out.println(arrKal[i]+" => "+arrCon[i]);
-    
+    }
+    private static void tampilHasil(String convert) {
+        System.out.println("Kalimat Alay Angka : " + convert);
+    }
+    public static void main(String[] args) {
+        String identitas = "MUHAMMAD BAGAS RAMADHAN / XRPL2 / 21";
+        
+        tampilJudul(identitas);
+        String kalimat = tampilInput();
+        String convert = vocal2Angka(kalimat);
+        tampilPerkata(kalimat, convert);
+        tampilHasil(convert);
     }
     
     
